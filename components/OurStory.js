96f5b6e07e46827f5a4b2d273f73b689292ee1b1
Regina B <rsbaher@ucalgary.ca>
2021-11-29 20:40:20 -0700

    fixed the our engagement section by removing styled components

--- components/OurStory.js
+++ components/OurStory.js
@@ -1,50 +1,21 @@
-import styled from 'styled-components'
 import Subtitle from '../components/subtitle'
 import Link from 'next/link'
 import Image from 'next/image'
 import HeartSvg from '../public/img/heart.svg'
 import SideImg from '../public/img/engagement/FadedLaugh.JPG'
-const Story = styled.p`
-  color: #6f6e77;
-  font-family: 'El Messiri', sans-serif;
-  font-size: 1.2em;
-  line-height: 1.5em;
-  margin: auto;
-  max-width: 750px;
-  padding: 0 2em;
-  text-align: center;
-  
 
-  @media (max-width: 414px) {
-    padding: 0 1em;
-  }
-`
-
-const SideImgDiv = styled.div`
-  width: 100%;
-  position: relative;
-  margin: auto;
-`
-const FadeContainer = styled.div`
-  width: 95%;
-	position: absolute;
-	bottom: 0px;
-	top: 0px;
-	box-shadow: inset 0px 0px 10px 20px #fff3e6;
-  padding-right: 5%;
-`
 
 export default function OurStory({ children }) {
   return (
     <>
       <Subtitle style={{ marginBottom: '0px' }}>Our Engagement</Subtitle>
       <div className="container-img-two-columns" style={{ maxWidth: '1300px' }}>
-        <SideImgDiv className="img-column">
+        <div className="side-img img-column">
           <Image className="width95" src={SideImg} />
-          <FadeContainer></FadeContainer>
-        </SideImgDiv>
+          <div className="faded-side-img"></div>
+        </div>
         <section className="text-column">
-          <Story>
+          <p className="story">
             <br />
             <Image src={HeartSvg} width={26} height={26} alt="Cartoon icon of a grey heart"/>
             <br />
@@ -63,7 +34,7 @@ export default function OurStory({ children }) {
                 <button type="button" className="button-style button-gold button-pink-hover color-light-gold">Gallery</button>
               </Link>
             </div>
-          </Story>
+          </p>
         </section>
       </div>
     </>
