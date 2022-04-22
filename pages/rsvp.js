8e1a49e53d2677fa6f479c95cf5dd38a93c07a4d
Regina B <reginabaher@gmail.com>
2022-04-21 20:49:21 -0600

    made form thinner

--- pages/rsvp.js
+++ pages/rsvp.js
@@ -5,11 +5,11 @@ import Layout from '../components/layout'
 export default function Gallery() {
   return (
     <Layout h1="Marco &amp; Regina" h2="">
-      <section className="content1 padding-bottom-2">
+      <section className="content1">
         <h2 className="color-gold" style={{fontSize: '4.5em'}}>R . S . V . P</h2>
         <div className="container-one-column">
           <iframe className="cards-div background-color-light-pink" src="https://docs.google.com/forms/d/e/1FAIpQLSe4T6BwFCA3Bw8TAhbWgqu9vuVdW3H3UiENOB3LnKN3WaCJXA/viewform?embedded=true"
-            width="400" height="1620">Loading…</iframe>
+            width="365" height="1620">Loading…</iframe>
         </div>
 
         <br />
